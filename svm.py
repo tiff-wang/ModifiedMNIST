153ed3fbc6d41ff153ae280c75095a4d131ce2d0
tiff-wang <wangtiffany@gmail.com>
2018-03-14 17:14:52 -0400

    working svm.py

--- svm.py
+++ svm.py
@@ -9,25 +9,30 @@ from sklearn import preprocessing
 def normalizer(m):
 	return preprocessing.normalize(m, axis = 1, norm = 'l2')
 
-print('start')
+print('parsing train_x')
 x = np.loadtxt("./dataset/train_x.csv", delimiter=",") # load from text 
+
+print('parsing train_y')
 y = np.loadtxt("./dataset/train_y.csv", delimiter=",") 
+
+print('parsing test_x')
 x_test = np.loadtxt("./dataset/test_x.csv", delimiter=",") 
+
 x = x.reshape(-1, 64, 64) # reshape 
 y = y.reshape(-1, 1) 
 x_test = x_test.reshape(-1, 64, 64)
-norm_x = normalizer(x)
-norm_x_test = normalizer(x_test)
+#norm_x = normalizer(x)
+#norm_x_test = normalizer(x_test)
 print('data initialized')
 
 #first 100
-
-scipy.misc.imshow(x[0]) # to visualize only 
-
+x = x.reshape(-1, 4096)
 classifier = svm.LinearSVC()
-classifier.fit(norm_x[:100],y[:100])
-test_predict = classifier.predict(norm_x_test[:100])
+classifier.fit(x,y)
 
+x_test = x_test.reshape(-1, 4096)
+test_predict = classifier.predict(x_test)
 
-score = sk.f1_score(norm_x_test[:100], test_predict, average='micro')
-print('test' + str(score))
+arr = np.arange(len(test_predict))
+
+np.savetxt('predict_output.csv', np.dstack((arr, test_predict))[0], "%d,%d", header = "Id,Label", comments='')
